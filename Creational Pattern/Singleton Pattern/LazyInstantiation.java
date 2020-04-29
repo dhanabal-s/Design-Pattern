88df59ed017553d4ab86ce129915611a9ec8bb72
Dhanabal <43275764+DhanabalShanmugam@users.noreply.github.com>
2020-04-29 16:48:23 +0530

    Update LazyInstantiation.java

--- Creational Pattern/Singleton Pattern/LazyInstantiation.java
+++ Creational Pattern/Singleton Pattern/LazyInstantiation.java
@@ -7,8 +7,8 @@ class Shop {
     private Shop() {
     }
     
-     // Compare to Early Instation program i just make the changes at here.
-    // In Lazy Instatiation we create object for class when it is called first time
+     // Compare to Early Instantiation program, i just make the changes over here.
+    // In Lazy Instantiation, create object for class when it is called first time
     public static Shop getInstance() {
         // Check the object not created before 
         if(shopObj == null) {
@@ -22,7 +22,7 @@ class Shop {
         totalProductCount += productCount;
     }
 
-    // I am just return a product count for understnding
+    // just return a product count for understnding
     public void shopDetails(){
         System.out.println("Total Number of products in a shop:"+totalProductCount);
     }
@@ -35,7 +35,7 @@ class Shop {
 
 # This Program runs clearly, but we have problem.
 # Think about multithreading , what happen if two threads call getInstance() at same time.
-# Yeah, you are right two object are created. So, solve this we use Synchronization technique.
+# Yeah, you are right two object are created. Solve this, we use Synchronization technique.
 
     // just add synchronized keyword with method name getInstance
     synchronized public static Shop getInstance() {
@@ -48,10 +48,10 @@ class Shop {
 
    
 # But using above method we have performance issue. So, solve that we use Double Check Locking method. 
-# In this method we just apply synchronized method divide into the sub part. See below Implementation
+# In below, just apply synchronized method divide into the sub part.
 
 public static Shop getInstance() {
-        // Check the object not created before 
+        // Check the object, if it not created before 
         if (shopObj == null) {
            synchronized(Shop.class){
              if(shopObj == null){
@@ -62,4 +62,4 @@ public static Shop getInstance() {
         return shopObj;
     }
 
-# Using this we avoid multithreading problem and also shortout performance issue
+# Using this, avoid multithreading problem and performance issue.
