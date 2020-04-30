9aaeb07e30e251912cb94f23ad97a83a3d3eaeea
Dhanabal <43275764+DhanabalShanmugam@users.noreply.github.com>
2020-04-30 14:44:15 +0530

    Update FactoryPattern.java

--- Creational Pattern/Factory Pattern/FactoryPattern.java
+++ Creational Pattern/Factory Pattern/FactoryPattern.java
@@ -4,10 +4,10 @@ public class FactoryPattern {
   
     public static void main(String[] args) {
         
-        // select bus for testing
+        // Select bus for testing
         String travelBy= new String("Bus");
         
-        // Client get the object specific travel class by calling Factory class
+        // Client get the object of specific travel class(bus, train,flight) by calling Factory class
         Travel travelObj = Factory.getInstance(travelBy);
         travelObj.book();
         
@@ -15,22 +15,22 @@ public class FactoryPattern {
     
 }
 
-// which returns the object of the specific class depends on the user request
+// It returns the object of the specific class depends on the user request
 class Factory {
     
     public static Travel getInstance(String travelBy) {
         
         switch (travelBy.toLowerCase()) {
-            // if user choice bus it returns the BusTravel object
+            // If user choice is bus, it returns the BusTravel object
             case "bus": return new BusTravel();
            
-            // if user choice is Train it returns the TrainTravel object
+            // If user choice is Train, it returns the TrainTravel object
             case "train": return new TrainTravel();
                 
-            // if user choice is Flight it returns the FlightTravel object    
+            // If user choice is Flight, it returns the FlightTravel object    
             case "flight": return new FlightTravel();
                 
-            // if user choice is undefined it returns null
+            // If user choice is undefined, it returns null
             default : return null;
         }
     }
@@ -41,7 +41,7 @@ interface Travel {
     void book();
 }
 
-// implemented the bus travel class by overridding travel interface methods
+// implementing the bus travel class by overridding travel interface methods
 class BusTravel implements Travel{
 
     @Override
@@ -49,11 +49,11 @@ class BusTravel implements Travel{
       System.out.println("Booked in Bus Travel");
     }
     
-    // other methods of bus travel
+    // Other methods of bus travel
     
 }
 
-// implemented the train travel class by overridding travel interface methods
+// implementing the train travel class by overridding travel interface methods
 class TrainTravel implements Travel{
 
     @Override
@@ -61,11 +61,11 @@ class TrainTravel implements Travel{
       System.out.println("Booked in Train Travel");
     }
     
-    // other methods of train travel
+    // Other methods of train travel
     
 }
 
-// implemented the flight travel class by overridding travel interface methods
+// implementing the flight travel class by overridding travel interface methods
 class FlightTravel implements Travel{
 
     @Override
@@ -73,6 +73,6 @@ class FlightTravel implements Travel{
       System.out.println("Booked in Flght Travel");
     }
     
-    // other methods of flight travel
+    // Other methods of flight travel
     
 }
